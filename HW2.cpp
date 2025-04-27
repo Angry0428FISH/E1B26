52b2b802fe6635a4d3dfb9820a7eed406e8d613e
E1B26 <angela.ly.hsu@gmail.com>
2025-04-27 20:48:09 +0800

    sceond commit
    因為要使用getch()，所以加入#include <conio.h>
    顯示主選單 (MENU 標籤)，提供使用者三個選項
    	(1)加入fflush(stdin)，避免讀到 Enter
    1. - A/a: 繪製靠右排列的直角三角形 (輸入 'a'-'n' 字母範圍)
    	(1)需加入space的for迴圈，才會顯示空白。
       -
       -
    2.根據使用者輸入的選項 (choice)，使用 switch-case 判斷並執行對應功能。
    3.每個功能完成後，按任意鍵返回主選單，清除畫面後重新顯示。
    	(1)先執行system("CLS")，再執行getch()，導致沒有出現"按任意鍵返回主選單"。
    4.輸入錯誤。
    	(1)沒有提示重新輸入，原因為 do-while迴圈內有 system("cls")清除畫面。

--- HW2.cpp
+++ HW2.cpp
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define PASSWORD 2025
+#include <conio.h> //getch()
 int main()
 {
+	//01
 	int i, pass;
-	
 	printf("請輸入4個數字的密碼:");
 	scanf("%d",&pass);
 	for(i=0;i<=3;i++)
@@ -18,16 +19,63 @@ int main()
 		}
 		if(i==3 && pass!=PASSWORD) return 1;		
 	}
-	printf("輸入正確\n");
-	
+	printf("輸入正確\n");	
 	system("pause");
 	system("CLS");
 	
+	//02
+MENU:
 	printf("--------------------------\n");
 	printf("|   A.a. 畫出直角三角形  |\n");
 	printf("|   B.b. 顯示乘法表      |\n");
 	printf("|   C.c. 結束            |\n");
 	printf("--------------------------\n");
+	char choice;
+	printf("輸入選項:");
+	fflush(stdin); //避免讀到 Enter。
+	scanf("%c",&choice);
+	
+	//03
+	switch(choice)
+	{
+		case 'A':
+		case 'a':
+		{
+			char ch;
+			system("cls");
+			printf("輸入一個‘a’到‘n’的字元:");
+            do 
+			{
+                
+                fflush(stdin);
+                scanf("%c", &ch);
+//三角形格式錯誤， 
+                if (ch >= 'a' && ch <= 'n') 
+				{
+                    for (char i = ch; i >= 'a'; i--) 
+					{
+						for (char space = 'a'; space < i; space++) // 印出空格，對齊右側
+						{
+							printf(" ");
+						}
+                        for (char j = i; j <= ch; j++) 
+						{
+                            printf("%c",j); 
+                        }
+                        printf("\n");
+                    }
+                } 
+				else
+					printf("輸入錯誤!請輸入一個‘a’到‘n’的字元:"); //輸入錯誤，沒有顯示，原因: do-while迴圈內有 system("cls") 
+            } while (!(ch >= 'a' && ch <= 'n'));
+
+            printf("\n按下任意建已回到主選單\n");
+            getch();
+			system("CLS");
+            goto MENU;
+		}
+	}
+	
 	
 	return 0;
 }
